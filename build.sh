eb46e98800678c5e9fd009331366286e8fc945d0
shunirr <git@s5r.jp>
2025-02-28 10:04:55 +0900

    remove yml file

--- build.sh
+++ build.sh
@@ -4,5 +4,4 @@ set -euo pipefail
 pip install -r requirements.txt
 python ./app.py
 
-cp openapi.yml ./dist/openapi.yml
 npx @redocly/cli build-docs "./dist/openapi.yml" --output "./dist/index.html"
