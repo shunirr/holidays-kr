4e52e2cdcaa211b95268ab40274678513d76318e
shunirr <git@s5r.jp>
2025-02-28 10:02:33 +0900

    add openapi.yml

--- app.py
+++ app.py
@@ -7,12 +7,24 @@ import json
 import os
 
 
+def generate_ics(date_list) -> str:
+    cal = Calendar()
+    for i in date_list:
+        event = Event()
+        event.name = i["name"]
+        event.begin = arrow.get(i["date"], "YYYY-MM-DD").replace(tzinfo="Asia/Seoul")
+        event.end = arrow.get(i["date"], "YYYY-MM-DD").replace(tzinfo="Asia/Seoul")
+        event.make_all_day()
+        cal.events.add(event)
+    return str(cal)
+
+
 def main():
     kr_holidays = holidays.country_holidays("KR")
-    base_dir = "dist/api/v1/"
+    base_dir = "dist/"
 
-    date_map = {}
-    datetime_map = {}
+    date_list = []
+    datetime_list = []
     now = datetime.date.today()
     for k in reversed(range(0, 3)):
         current_year = now.year - k
@@ -22,38 +34,38 @@ def main():
                 current_date = datetime.datetime(current_year, i, j)
                 holiday = kr_holidays.get(current_date)
                 if holiday:
-                    date_map[current_date.strftime("%Y-%m-%d")] = holiday
-                    datetime_map[str(int(current_date.timestamp()))] = holiday
+                    for l in holiday.split("; "):
+                        date_list.append(
+                            {"date": current_date.strftime("%Y-%m-%d"), "name": l}
+                        )
+                        datetime_list.append(
+                            {
+                                "datetime": int(current_date.timestamp()),
+                                "name": l,
+                            }
+                        )
 
     os.makedirs(base_dir, exist_ok=True)
 
     # Generate ics file
-    cal = Calendar()
-    for k, v in date_map.items():
-        event = Event()
-        event.name = v
-        event.begin = arrow.get(k, "YYYY-MM-DD").replace(tzinfo="Asia/Seoul")
-        event.end = arrow.get(k, "YYYY-MM-DD").replace(tzinfo="Asia/Seoul")
-        event.make_all_day()
-        cal.events.add(event)
-
+    ics_data = generate_ics(date_list)
     with open(base_dir + "holidays.ics", "w") as outfile:
-        outfile.write(str(cal))
+        outfile.write(ics_data)
 
     # Write json and csv files
     with open(base_dir + "date.json", "w") as outfile:
-        json.dump(date_map, outfile, ensure_ascii=False, indent=2)
+        json.dump(date_list, outfile, ensure_ascii=False, indent=2)
 
     with open(base_dir + "date.csv", "w") as outfile:
-        for k, v in date_map.items():
-            outfile.write(f"{k},{v}\n")
+        for i in date_list:
+            outfile.write(f"{i['date']},{i['name']}\n")
 
     with open(base_dir + "datetime.json", "w") as outfile:
-        json.dump(datetime_map, outfile, ensure_ascii=False, indent=2)
+        json.dump(datetime_list, outfile, ensure_ascii=False, indent=2)
 
     with open(base_dir + "datetime.csv", "w") as outfile:
-        for k, v in datetime_map.items():
-            outfile.write(f"{k},{v}\n")
+        for i in datetime_list:
+            outfile.write(f"{i['datetime']},{i['name']}\n")
 
 
 if __name__ == "__main__":
--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+set -euo pipefail
+
 pip install -r requirements.txt
 python ./app.py
+
+cp openapi.yml ./dist/openapi.yml
+npx @redocly/cli build-docs "./dist/openapi.yml" --output "./dist/index.html"
